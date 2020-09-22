616dfcd90a3d8eea121e2f4e709425f859b8889c
Soumik Ranjan Dasgupta <20687315+srdg@users.noreply.github.com>
2020-09-22 13:07:55 +0000

    Update comics-download.py

--- comics-download.py
+++ comics-download.py
@@ -1,15 +1,24 @@
 from bs4 import BeautifulSoup as bs
 import requests
 from fpdf import FPDF
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", help="Comic Name")
+parser.add_argument("-o", help="Offset (e.g. ....online-free-0284.jpg will have this argument as 284 (page number in URL for first page of comic)", type=int)
+parser.add_argument("-n", help="Number of pages in comic book", type=int)
+parser.add_argument("-f", help="Number of digits in a page number (e.g. ....online-free-0284.jpg will have this argument as 4 (digits)", type=int)
+args = parser.parse_args()
 
 
 def downloadPages():
-	parent = "https://www.omgbeaupeep.com/comics/mangas/Avatar The Last Airbender/011 - Avatar The Last Airbender - Smoke and Shadow Part 2 (2015)/read-avatar-the-last-airbender-comics-online-free-"
+	parent = "https://www.omgbeaupeep.com/comics/mangas/Avatar The Last Airbender/"+args.s+"/read-avatar-the-last-airbender-comics-online-free-"
 	page_list = []
-	for i in range(1,81):
-		img_data = requests.get(parent+str(i).zfill(3)+'.jpg').content
-		root_dir='Book 7/'
-		
+	for i in range(1,args.n+1):
+		img_data = requests.get(parent+str(i+(args.o-1)).zfill(args.f)+'.jpg').content
+		root_dir='Book/'
+		Path(root_dir).mkdir(parents=True, exist_ok=True) 
 		with open(root_dir+str(i).zfill(3)+'.jpg', 'wb') as handler:
 			page_list.append(root_dir+str(i).zfill(3)+'.jpg')
 			handler.write(img_data)
@@ -22,13 +31,15 @@ def createFile(page_list):
 	for page in page_list:
 		pdf.add_page()
 		pdf.image(page, 0,0,210,297)
-	pdf.output("Book 14 - Smoke and Shadow-II.pdf", "F")
+	pdf.output(args.s+".pdf", "F")
 	
 	
 
 def main():
 	page_list = downloadPages()
 	createFile(page_list)
-	
+	print(args.s, type(args.s))
+	print(args.n, type(args.n))
+	print(args.f, type(args.f))
 if __name__=="__main__":
 	main()
